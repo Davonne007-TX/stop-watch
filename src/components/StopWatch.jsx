dee2d7c7c8e57a13ecfed00a4db9da18b6af787e
Vonney <davonnejv@gmail.com>
2024-01-03 14:48:49 -0600

    adding tailwind styles

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -1,7 +1,15 @@
+import { useRef, useState } from "react";
+
 export default function StopWatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
+
   return (
     <>
-      <h1 className="text-white">Stop Watch</h1>
+      <div className="text-white flex h-screen items-center text-7xl">
+        <span className="mx-auto">Time:</span>
+      </div>
     </>
   );
 }
