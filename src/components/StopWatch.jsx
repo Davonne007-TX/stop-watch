a2dfb4d84492c8447ba5e3d653f7a03aff76f071
Vonney <davonnejv@gmail.com>
2024-01-03 15:09:34 -0600

    adding styles to buttons

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -7,8 +7,19 @@ export default function StopWatch() {
 
   return (
     <>
-      <div className="text-white flex h-screen items-center text-7xl">
+      <div className="text-white flex flex-col items-center text-7xl mt-80">
         <span className="mx-auto">Time:</span>
+        <div className="flex gap-4 bg-white p-5 text-5xl mt-5">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+            Start
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+            Stop
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+            Reset
+          </button>
+        </div>
       </div>
     </>
   );
